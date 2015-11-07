4c5f9ed90d60b9089f5de0e6a186bb2ddb1ee662
Jeremy Burns <j@jaburns.net>
2015-11-07 16:00:04 -0700

    Added settling algorithm to generated forest

--- Assets/Scripts/ForestGenerator.cs
+++ Assets/Scripts/ForestGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class ForestGenerator : MonoBehaviour
 {
@@ -6,14 +7,20 @@ public class ForestGenerator : MonoBehaviour
     public Vector2 SpawnRange;
     public int SpawnCount;
 
+    List<TreeController> _trees;
+
     void Awake()
     {
+        _trees = new List<TreeController>();
+
         for (int i = 0; i < SpawnCount; ++i) {
-            spawnTree(new Vector2 {
+            _trees.Add(spawnTree(new Vector2 {
                 x = (.5f - Random.value) * SpawnRange.x,
                 y = (.5f - Random.value) * SpawnRange.y
-            });
+            }));
         }
+
+        settleTrees(_trees);
     }
 
     TreeController spawnTree(Vector2 pos)
@@ -26,5 +33,43 @@ public class ForestGenerator : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(transform.position, SpawnRange.AsVector3());
+    }
+
+    void FixedUpdate()
+    {
+        settleTrees(_trees);
+    }
+
+    static void settleTrees(List<TreeController> allTrees)
+    {
+        var trees = allTrees; //new List<TreeController>(allTrees);
+
+        foreach (var tree0 in trees) {
+            // Find the closest other tree to tree0.
+            TreeController closestTree = null;
+            float closestTreeDist = float.MaxValue;
+
+                Debug.Log(trees.Count);
+            foreach (var tree1 in trees) {
+                if (tree0 == tree1) continue;
+                var d2 = (tree0.transform.position - tree1.transform.position).sqrMagnitude;
+                if (d2 < closestTreeDist) {
+                    closestTreeDist = d2;
+                    closestTree = tree1;
+                }
+            }
+
+            if (closestTree == null) continue;
+
+            clumpTrees(tree0, closestTree);
+        }
+    }
+
+    static void clumpTrees(TreeController a, TreeController b)
+    {
+        var shift = (a.transform.position - b.transform.position) * 0.1f;
+        Debug.Log(shift);
+        a.transform.position += shift;
+        b.transform.position -= shift;
     }
 }
