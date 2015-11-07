47f3a2cedd57f0326404b2a0d793f53242d5daca
Jeremy Burns <j@jaburns.net>
2015-11-07 16:15:59 -0700

    Added forest generator

--- Assets/Scripts/ForestGenerator.cs
+++ Assets/Scripts/ForestGenerator.cs
@@ -19,8 +19,6 @@ public class ForestGenerator : MonoBehaviour
                 y = (.5f - Random.value) * SpawnRange.y
             }));
         }
-
-        settleTrees(_trees);
     }
 
     TreeController spawnTree(Vector2 pos)
@@ -33,43 +31,5 @@ public class ForestGenerator : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(transform.position, SpawnRange.AsVector3());
-    }
-
-    void FixedUpdate()
-    {
-        settleTrees(_trees);
-    }
-
-    static void settleTrees(List<TreeController> allTrees)
-    {
-        var trees = allTrees; //new List<TreeController>(allTrees);
-
-        foreach (var tree0 in trees) {
-            // Find the closest other tree to tree0.
-            TreeController closestTree = null;
-            float closestTreeDist = float.MaxValue;
-
-                Debug.Log(trees.Count);
-            foreach (var tree1 in trees) {
-                if (tree0 == tree1) continue;
-                var d2 = (tree0.transform.position - tree1.transform.position).sqrMagnitude;
-                if (d2 < closestTreeDist) {
-                    closestTreeDist = d2;
-                    closestTree = tree1;
-                }
-            }
-
-            if (closestTree == null) continue;
-
-            clumpTrees(tree0, closestTree);
-        }
-    }
-
-    static void clumpTrees(TreeController a, TreeController b)
-    {
-        var shift = (a.transform.position - b.transform.position) * 0.1f;
-        Debug.Log(shift);
-        a.transform.position += shift;
-        b.transform.position -= shift;
     }
 }
