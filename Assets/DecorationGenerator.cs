de376c9758ed6bd3332395e22b1d10282b4f1c6b
Walter Gordy <walter.gordy@gmail.com>
2015-11-08 03:47:48 -0700

    Forest Generation

---
+++ Assets/DecorationGenerator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DecorationGenerator : MonoBehaviour {
+
+    public GameObject ThingPrefab;
+    public Vector2 SpawnRange;
+    public int SpawnCount;
+    public float SmallSize;
+    public float LargeSize;
+
+    void Awake()
+    {
+        for (int i = 0; i < SpawnCount; ++i)
+        {
+            spawnThing(new Vector2
+            {
+                x = (.5f - Random.value) * SpawnRange.x,
+                y = (.5f - Random.value) * SpawnRange.y
+            });
+        }
+    }
+
+    void spawnThing(Vector2 pos)
+    {
+        var thingObj = Instantiate(ThingPrefab, pos.AsVector3(), Quaternion.identity) as GameObject;
+        thingObj.transform.localScale *= Random.value * (LargeSize - SmallSize) + SmallSize;
+        thingObj.transform.parent = transform;
+        thingObj.transform.rotation = Quaternion.EulerAngles(0, 0, Random.value * 2000);
+    }
+}
