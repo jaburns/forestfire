1d83e35a5459ead533b4489abc29bf4faf6664bf
Walter Gordy <walter.gordy@gmail.com>
2015-11-08 11:03:50 -0700

    Added title stuff

---
+++ Assets/EnableNextImage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableNextImage : MonoBehaviour {
+    public GameObject nextImage;
+    public float delay;
+	// Use this for initialization
+	void Start () {
+        Invoke("NextImage", delay);
+	}
+
+    void NextImage()
+    {
+        nextImage.SetActive(true);
+    }
+}
