3a8ccc493d0f63c6fe7dbf073ee205060e8a0d69
Walter Gordy <walter.gordy@gmail.com>
2015-11-07 21:46:50 -0700

    Adds forest fires and tree sprites

---
+++ Assets/EmmisionRateRange.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmmisionRateRange : MonoBehaviour {
+
+    public float LowRate = 1;
+    public float HighRange = 20;
+
+	// Use this for initialization
+	void Start () {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        ps.emissionRate = (int)Random.Range(LowRate, HighRange);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/SpriteRenderLayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteRenderLayer : MonoBehaviour {
+    public string layer = "Foreground";
+    // Use this for initialization
+    void Start () {
+        ParticleSystem sr = GetComponent<ParticleSystem>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
