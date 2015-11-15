70a76c856c5cf26fe997f62e0f7d97aeef7e3f45
Walter Gordy <walter.gordy@gmail.com>
2015-11-15 14:09:58 -0700

    Changes music manager to work while scenes are loading and tries to seamlessly continue music between the title track and the music track for the game.

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+    public static MusicManager instance = null;
+    public AudioClip titleTrack;
+    public AudioClip gameTrack;
+    public AudioClip endingTrack;
+
+    AudioSource audioSource = null;
+
+    public enum trackType { title, game, win};
+
+    trackType currentTrack;
+    trackType trackToPlay;
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+            audioSource = GetComponent<AudioSource>();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+
+    }
+	
+    public void SetTrack(trackType track)
+    {
+        switch (track)
+        {
+            case trackType.title:
+                playTrack(titleTrack);
+                break;
+            case trackType.game:
+                playTrack(gameTrack, audioSource.time);
+                break;
+            case trackType.win:
+          //      playTrack(endingTrack);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void playTrack(AudioClip clip, float offset = 0)
+    {
+        if (clip == null)
+            return;
+        audioSource.clip = clip;
+        audioSource.time = offset;
+        audioSource.Play();
+    }	
+}
---
+++ Assets/SetTrack.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetTrack : MonoBehaviour {
+    public MusicManager.trackType sceneTrack = MusicManager.trackType.title;
+    
+    void Start () {
+        MusicManager.instance.SetTrack(sceneTrack);
+	}
+	
+}
