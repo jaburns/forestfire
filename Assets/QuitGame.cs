de217aae370c9c2557df25dfc6ac8a6fd08dfc9d
Walter Gordy <walter.gordy@gmail.com>
2015-11-20 08:01:19 -0700

    MADJAM Release, Escape button quits game

---
+++ Assets/QuitGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
